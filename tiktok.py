99836f25eec946865496fb040fe3e471a6d454de
Ubiquitous-X <ubiquitous@bahnhof.se>
2024-10-13 12:11:47 +0200

    updated

--- tiktok.py
+++ tiktok.py
@@ -1,7 +1,15 @@
 from TikTokLive import TikTokLiveClient
-from TikTokLive.types.events import CommentEvent, ConnectEvent, JoinEvent, LiveEndEvent
+from TikTokLive.events import CommentEvent, ConnectEvent, JoinEvent, LiveEndEvent
 from mctools import RCONClient
 import sys
+
+# Check if livstream user argument has been sent
+if len(sys.argv) > 1:
+    livestream = sys.argv[1]
+else:
+    # Om inget argument skickas, kasta ett fel och avsluta scriptet
+    print("Error: No username sent. Start the script with 'python tiktokpy username' (without @)")
+    sys.exit(1)  # Exit with error code 1
 
 rcon = RCONClient('localhost') # The adress of the Minecraft server
 
@@ -13,30 +21,28 @@ else:
     print("Couldn't connect to RCON")
     exit()
 
-livestream = "username" # The username of the live streamer, without the '@' sign 
-
 # Instantiate the client with the user's username
-client: TikTokLiveClient = TikTokLiveClient(unique_id="@"+livestream, debug=1)
+client: TikTokLiveClient = TikTokLiveClient(unique_id="@"+livestream)
 
 # Define how you want to handle specific events via decorator
-@client.on("connect")
+@client.on(ConnectEvent)
 async def on_connect(_: ConnectEvent):
     print("Connected to Room ID:", client.room_id)
 
-@client.on("join")
+@client.on(JoinEvent)
 async def on_join(event: JoinEvent):
     print(f"{event.user.nickname} joined the stream")
     # Spawn a zombie named as the viewer who just joined the stream
     rcon.command("title @a actionbar {\"text\":\""+event.user.nickname+" joined the stream\", \"color\":\"aqua\"}")
     rcon.command("execute at @p run summon zombie ^5 ^ ^ {CustomNameVisible:1,CustomName:'{\"text\":\""+event.user.nickname+"\"}'}")
 
-@client.on("comment")
+@client.on(CommentEvent)
 async def on_comment(event: CommentEvent):
     # Show a comment on the actionbar title with green text
     rcon.command("title @a actionbar {\"text\":\""+event.user.nickname+" says: "+event.comment+"\", \"color\":\"green\"}")
     print(f"{event.user.nickname} -> {event.comment}")
 
-@client.on("live_end")
+@client.on(LiveEndEvent)
 async def on_connect(event: LiveEndEvent):
     print(f"Livestream ended :(")
 
